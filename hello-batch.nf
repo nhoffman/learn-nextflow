723fa9927987786b52336eb8c28a997a6934e398
Noah Hoffman <noah.hoffman@gmail.com>
2019-07-28 11:09:54 -0700

    batch still failing

--- hello-batch.nf
+++ hello-batch.nf
@@ -1,15 +1,25 @@
 #!/usr/bin/env nextflow
 
+infile = file(params.infile)
+outfile = infile.getSimpleName() + '.md5'
 
 process md5sum {
+    publishDir "$params.outdir/"
+
     container "ubuntu:18.04"
     cpus 1
     memory "2 GB"
 
     input:
-	file infile from params.infile
+	file input from infile
+    	val output from outfile
+
+    output:
+	file output into md5
 
     """
-    md5sum $infile
+    md5sum $input > $output
     """
 }
+
+md5.subscribe { print "$it" }
--- run-hello-batch.sh
+++ run-hello-batch.sh
@@ -5,14 +5,22 @@ set -e
 # ml nextflow/19.04.1
 
 BASE_BUCKET="s3://fh-pi-fredricks-d/lab/nhoffman/$(basename $(pwd))"
-infile="$BASE_BUCKET/hello-in.txt"
+# infile="$BASE_BUCKET/hello-in.txt"
+infile="local_inputs/hello-in.txt"
 
-# aws s3 cp local_inputs/hello-in.txt $infile
+# Question:
+# reproducible-workflows/nextflow/align-proteins-diamond/align-proteins-diamond.nf
+# does not include -bucket-dir - why not?
+
+# Question: why does the container need to be specified both here
+# ("-with-docker") and in the process definition?
 
 nextflow \
     run \
-    hello.nf \
+    hello-batch.nf \
     --infile "$infile" \
+    --outdir $BASE_BUCKET/output \
     -bucket-dir $BASE_BUCKET \
+    -c nextflow-batch.config \
     -with-docker "ubuntu:18.04" \
     -resume
